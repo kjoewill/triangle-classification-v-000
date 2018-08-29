d243259d8d0265cd96e6b0bbc0d6b40dbd38ec1a
Kevin W <kjoewill@gmail.com>
2018-08-29 20:40:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  
+  
+  
+  
+  
+  def kind 
+    
+  end
+  
+  
+  private
+  
+  isIs
+  
 end
