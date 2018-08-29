2c264875ac50868204fe9df1132cb314aafd5c8d
Kevin W <kjoewill@gmail.com>
2018-08-29 20:54:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
   end
   
   def is_isosceles?
-    if @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+    @side1 == @side2 || @side1 == @side3 || @side2 == @side3
   end
   
   def no_zero_sides?
