304b9968900ac061bbb4c65369d0bde9df940354
Kevin W <kjoewill@gmail.com>
2018-08-29 20:51:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,8 +18,16 @@ class Triangle
   
   private
   
+  def is_equilateral?
+    @side1 == @side2 && @side1 == @side3 
+  end
+  
   def is_isosceles?
-    if @side1 = @side2 || @side1 = @side3 || @side2 = @side3
+    if @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+  end
+  
+  def no_zero_sides?
+    @side1 > 0 && @side2 > 0 && @side3 > 0
   end
   
 end
